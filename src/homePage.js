4945a4ac5c6ad89157975eccd093ab84fe52aa87
naftali2055 <naftali2055@gmail.com>
2023-09-01 18:02:42 +0300

    first commit

---
+++ src/homePage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    toJewishDate,
+    formatJewishDateInHebrew,
+  } from "jewish-date";import "./css/homePage.css"
+
+const HomePage = ({brideName, groomName, weddingDate}) => {  
+    const jewishDate = toJewishDate(weddingDate);
+    // const jewishDateInHebrew = toHebrewJewishDate(jewishDate);
+    const jewishDateInHebrewStr = formatJewishDateInHebrew(jewishDate);
+    return (
+      <div className="homepage">
+        <header className="header">
+          <h1>החתונה של</h1>
+          <h2>{brideName} & {groomName}</h2>
+          <p>שמרו את התאריך</p>
+          <p> {jewishDateInHebrewStr} - {weddingDate.toJSON().split('T')[0]}</p>
+        </header>
+      </div>
+    );
+  }
+
+  export default HomePage
